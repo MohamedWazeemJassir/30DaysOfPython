8f5f3d63ce12349727e9b7a1f747939cf663bf26
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2025-12-09 21:20:03 +0530

    first commit

---
+++ Day001/calculator.py
@@ -0,0 +1,63 @@
+# Get user intent
+print("""
+1. Addition \n
+2. Subtraction \n
+3. Multiplication \n
+4. Division \n
+5. Press 5 to exit.
+""")
+
+calculator_condition = True
+
+while calculator_condition:
+	print("\n")
+	print("Welcome to calculator !")
+
+	user_operation = int(input("Enter your option: "))
+
+	if user_operation == 1:
+		# Addition Logic
+		print("Addition")
+		number_1 = int(input(" Enter number 1 "))
+		number_2 = int(input(" Enter number 2 "))
+		result = number_1 + number_2
+		print("The result is", result)
+		
+	elif user_operation == 2:
+		# Subtraction Logic
+		print("Subtract")
+		number_1 = int(input(" Enter number 1 "))
+		number_2 = int(input(" Enter number 2 "))
+		result = number_1 - number_2
+		print("The result is", result)
+		
+	elif user_operation == 3:
+		# Multiplication Logic
+		print("Multiplication")
+		number_1 = int(input(" Enter number 1 "))
+		number_2 = int(input(" Enter number 2 "))
+		result = number_1 * number_2
+		print("The result is", result)
+		
+	elif user_operation == 4:
+		# Division Logic
+		print("Division")
+		number_1 = int(input(" Enter number 1 "))
+		number_2 = int(input(" Enter number 2 "))
+		result = number_1 / number_2
+		print("The result is", result)
+	
+	elif user_operation == 5:
+		print("Thanks for using calculator ! See you again.")
+		calculator_condition = False
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day002/grocery_bill_separator.py
@@ -0,0 +1,36 @@
+commodity_price = {
+	"rice": 40,
+	"wheat": 50,
+	"egg": 6,
+	"oil": 118,
+	"curd": 10,
+	"carrot": 90
+}
+
+total_items = int(input("Enter Total Grocery Items :"))
+print("Total Items", total_items)
+
+groceries = []
+user_picked_commodity = {}
+bill_items = []
+for itr in range(total_items):
+	grocery_name = input("Enter grocery item name: " + str(itr) + " ")
+	groceries.append(grocery_name)
+ 	
+	weight = float(input("Weight of " + grocery_name + " "))
+	user_picked_commodity[grocery_name] = weight * commodity_price.get(grocery_name, 0)
+ 	
+	billed_res = (
+ 					grocery_name,
+ 					weight,
+ 					commodity_price.get(grocery_name, 0),
+ 					user_picked_commodity.get(grocery_name)
+ 					)
+	bill_items.append(billed_res)
+
+
+ 
+print("Groceries collected ", user_picked_commodity)
+
+for item in bill_items:
+	print(item)
