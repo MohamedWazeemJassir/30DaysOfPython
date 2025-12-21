05997dadbdd6349b79bd0b45dfb98fb2d2d234f9
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2025-12-21 22:38:16 +0530

    Added advanced operations

--- Day01/calculator.py
+++ Day01/calculator.py
@@ -4,11 +4,15 @@ print("""
 2. Subtraction \n
 3. Multiplication \n
 4. Division \n
-5. Press 5 to exit.
+5. Modulus \n
+6. Power \n
+7. Floor Division \n
+8. Press 8 to exit.
 """)
 
 calculator_condition = True
 
+# Define functions for each operation
 def add():
 	number_1 = int(input(" Enter number 1 "))
 	number_2 = int(input(" Enter number 2 "))
@@ -36,6 +40,24 @@ def divide():
 		result = number_1 / number_2
 		print("The result is", result)
 
+def modulus():
+	number_1 = int(input(" Enter number 1 "))
+	number_2 = int(input(" Enter number 2 "))
+	result = number_1 % number_2
+	print("The result is", result)	
+
+def power():
+	number_1 = int(input(" Enter number 1 "))
+	number_2 = int(input(" Enter number 2 "))
+	result = number_1 ** number_2
+	print("The result is", result)	
+
+def floor_division():
+	number_1 = int(input(" Enter number 1 "))
+	number_2 = int(input(" Enter number 2 "))
+	result = number_1 // number_2
+	print("The result is", result)	
+
 while calculator_condition:
 	print("\n")
 	print("Welcome to calculator !")
@@ -61,8 +83,23 @@ while calculator_condition:
 		# Division Logic
 		print("Division")
 		divide()
-	
+		
 	elif user_operation == 5:
+		# Modulus Logic
+		print("Modulus")
+		modulus()
+		
+	elif user_operation == 6:
+		# Power Logic
+		print("Power")
+		power()
+		
+	elif user_operation == 7:
+		# Floor Division Logic
+		print("Floor Division")
+		floor_division()
+	
+	elif user_operation == 8:
 		print("Thanks for using calculator ! See you again.")
 		calculator_condition = False
 
