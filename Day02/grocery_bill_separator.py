655afee445efdece92875f5e2a45b3d524262ac6
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2025-12-30 21:02:10 +0530

    Completed Day 02 Assignment

--- Day02/grocery_bill_separator.py
+++ Day02/grocery_bill_separator.py
@@ -8,53 +8,62 @@ commodity_price = {
 	"carrot": 90
 }
 
-# Get number of items as input
-total_items = int(input("Enter Total Grocery Items :"))
-print("Total Items", total_items)
-
 groceries = []
 user_picked_commodity = {}
 bill_items = []
 
-for itr in range(total_items):
-	# Get item name and weight
-	grocery_name = input("Enter grocery item name: " + str(itr) + " ")
-	# Task 1 - Validate Commodity Name
-	# If the user enters an item not in commodity_price then show, "Item not available". 
-	# Ask the user to re-enter the item name.
-	if grocery_name not in commodity_price:
-		print("Item not available")
-		grocery_name = input("Enter grocery item name: " + str(itr) + " ")
-	weight = float(input("Weight of " + grocery_name + " "))
+# Task 5 - Create functions
+def display_items():
+	print("Items available are:")
+	for key, value in commodity_price.items():
+		print(key.capitalize())
+	print()
 
-	# Append item name to list an d calculate price
-	groceries.append(grocery_name)
-	user_picked_commodity[grocery_name] = weight * commodity_price.get(grocery_name, 0)
- 	
-	# Create each item's bill as tuple and apeend it to final bill list
-	billed_res = (
- 					grocery_name,
- 					weight,
- 					commodity_price.get(grocery_name, 0),
- 					user_picked_commodity.get(grocery_name)
- 					)
-	bill_items.append(billed_res)
+def get_user_items():
+	# Get number of items as input
+	total_items = int(input("Enter Total Grocery Items : "))
+	for itr in range(total_items):
+		# Get item name and weight
+		grocery_name = input("Enter grocery item name: " + str(itr + 1) + " ")
+		
+		# Task 1 - Validate Commodity Name
+		if grocery_name not in commodity_price:
+			print("Item not available")
+			grocery_name = input("Enter grocery item name: " + str(itr + 1) + " ")
+		weight = float(input("Enter weight of " + grocery_name + ": "))
 
-# Print the groceries collected
-print("Groceries collected ", user_picked_commodity)
+		# Append item name to list an d calculate price
+		groceries.append(grocery_name)
+		user_picked_commodity[grocery_name] = weight * commodity_price.get(grocery_name, 0)
+		
+		# Create each item's bill as tuple and append it to final bill list
+		billed_res = (
+						grocery_name,
+						weight,
+						commodity_price.get(grocery_name, 0),
+						user_picked_commodity.get(grocery_name)
+						)
+		bill_items.append(billed_res)
 
-# Task 2 - Find the grand total. 
-grand_total = 0
+def calculate_bill():
+	# Task 2 - Find the grand total. 
+	grand_total = 0
+	for item in bill_items:
+		grand_total += item[3]
+	# Task 4 - Add 5% GST on total amount on all commodities.
+	grand_total += grand_total * 0.05
+	return grand_total
 
-# Task 3 - Pretty Bill Format
-print("Item\tWeight\tPrice\tAmount")
-print("---------------------------------")
-for item in bill_items:
-	print(f"{item[0]}\t{item[1]}kg\t{item[2]}\t{item[3]}")
-	grand_total += item[3]
+def print_bill():
+	# Task 3 - Pretty Bill Format
+	print("\nItem\tWeight\tPrice\tAmount")
+	print("---------------------------------")
+	for item in bill_items:
+		print(f"{item[0]}\t{item[1]}kg\t{item[2]}\t{item[3]}")
+	print("---------------------------------\n")
+	print("Grand total: ", calculate_bill())
 
-# Task 4 - Add 5% GST on total amount on all commodities.
-grand_total += grand_total * 0.05
+display_items()
+get_user_items()
+print_bill()
 
-print("---------------------------------")
-print("Grand total: ", grand_total)
