0dd04efb2b6bdfe44921406bc973d038d895c473
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2026-01-04 22:50:58 +0530

    Edited Day15

--- Day15/add.py
+++ Day15/add.py
@@ -1,4 +1,2 @@
-
-
 def add_two_numbers(num1, num2):
 	return num1 + num2
--- Day15/app.py
+++ Day15/app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route("/<name>")
 def home(name):
-	# return "Hello World"
 	return render_template("home.html", name=name)
 
 @app.route("/add/<num1>/<num2>", methods=["GET", "POST"])
@@ -18,18 +17,13 @@ def add(num1, num2):
 def search():
 	name = request.args.get("name")
 	age = request.args.get("age")
-
 	return {"name": name, "age": age}
-
-
 
 @app.route("/api/user", methods=["POST"])
 def create_user():
 	data = request.json
 	data["message"] = "Processed"
 	return jsonify(data)
-
-
 
 if __name__ == "__main__":
 	app.run(debug=True)
