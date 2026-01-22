8422070fb71310bf0112506a2391af4fc20c7e1e
Mohamed Wazeem Jassir <mohamedwazeemjassir@gmail.com>
2026-01-22 23:10:56 +0530

    Completed Calculator Assignment

--- Day01/calculator.py
+++ Day01/calculator.py
@@ -1,63 +1,70 @@
 # Get user intent
 print("""
-1. Addition \n
-2. Subtraction \n
-3. Multiplication \n
-4. Division \n
-5. Modulus \n
-6. Power \n
-7. Floor Division \n
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Modulus
+6. View Calculation History
+6. Power
+7. Floor Division
 8. Press 8 to exit.
 """)
 
 calculator_condition = True
+history = []
 
 # Task 4 - Use functions
-	# 1. add(a, b)
-	# 2. subtract(a, b)
-	# 3. multiply(a, b)
-	# 4. divide(a, b)
+def store_calculations(operation_name, input_values, result):
+	history.append([operation_name, input_values, result])
+
 def add():
 	number_1 = int(input(" Enter number 1 "))
 	number_2 = int(input(" Enter number 2 "))
 	result = number_1 + number_2
 	print("The result is", result)
+	store_calculations("Add", (number_1, number_2), result)
 
 def subtract():
 	number_1 = int(input(" Enter number 1 "))
 	number_2 = int(input(" Enter number 2 "))
 	result = number_1 - number_2
 	print("The result is", result)
+	store_calculations("Subtract", (number_1, number_2), result)
 
 def multiply():
 	number_1 = int(input(" Enter number 1 "))
 	number_2 = int(input(" Enter number 2 "))
 	result = number_1 * number_2
 	print("The result is", result)
+	store_calculations("Multiply", (number_1, number_2), result)
 
 def divide():
 	number_1 = int(input(" Enter number 1 "))
 	number_2 = int(input(" Enter number 2 "))
+
 	# Task 1 - Handle Division by Zero. 
-	# If user tries to divide by 0, show a friendly message
 	if number_2 == 0:
 			print("Error: Division by zero is not allowed.")
 	else:
 		result = number_1 / number_2
 		print("The result is", result)
+		store_calculations("Divide", (number_1, number_2), result)
 
 # Task 3 - Add advanced operations (Modulus, Power, Floor Division)
 def modulus():
 	number_1 = int(input(" Enter number 1 "))
 	number_2 = int(input(" Enter number 2 "))
 	result = number_1 % number_2
-	print("The result is", result)	
+	print("The result is", result)
+	store_calculations("Modulus", (number_1, number_2), result)	
 
 def power():
 	number_1 = int(input(" Enter number 1 "))
 	number_2 = int(input(" Enter number 2 "))
 	result = number_1 ** number_2
 	print("The result is", result)	
+	store_calculations("Power", (number_1, number_2), result)
 
 def floor_division():
 	number_1 = int(input(" Enter number 1 "))
@@ -67,7 +74,7 @@ def floor_division():
 	else:
 		result = number_1 // number_2
 		print("The result is", result)
-	print("The result is", result)	
+		store_calculations("Floor Division", (number_1, number_2), result)
 
 while calculator_condition:
 	print("\n")
@@ -99,19 +106,27 @@ while calculator_condition:
 		# Modulus Logic
 		print("Modulus")
 		modulus()
-		
+
 	elif user_operation == 6:
+		print()
+		if history == []:
+			print("No calculations done yet.")
+		else:
+			for _ in history:
+				print(_)
+		
+	elif user_operation == 7:
 		# Power Logic
 		print("Power")
 		power()
 		
-	elif user_operation == 7:
+	elif user_operation == 8:
 		# Floor Division Logic
 		print("Floor Division")
 		floor_division()
 	
-	elif user_operation == 8:
-		print("Thanks for using calculator ! See you again.")
+	elif user_operation == 9:
+		print("Thanks for using calculator! See you again.")
 		calculator_condition = False
 
 	# Task 2 - Invalid Choice Handling: 
